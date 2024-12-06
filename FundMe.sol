cd15633384fbf669020c01d0b45c150cd6cb5a55
UdayPandey01 <pandeyuday113@gmail.com>
2024-12-06 16:37:54 +0530

    fundMe

---
+++ FundMe.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+contract FundMe {
+    uint256 public minimumUsd = 50 * 1e18;
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't have enough funds");
+    }
+    // function withdraw() public {}
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10) ;
+    }
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
